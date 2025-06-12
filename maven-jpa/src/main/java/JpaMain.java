1e39f305202717dc293848327046bc73666db2b1
jkchappydev <jkchappydev@gmail.com>
2025-06-13 02:52:43 +0900

    study: section8 - 상속관계 매핑

--- maven-jpa/src/main/java/JpaMain.java
+++ maven-jpa/src/main/java/JpaMain.java
@@ -1,11 +1,10 @@
-import jpabook.jpashop.domain.Member;
-import jpabook.jpashop.domain.Team;
+import jpabook.jpashop.domain.Item;
+import jpabook.jpashop.domain.Movie;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -17,6 +16,25 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Movie movie = new Movie();
+            // Movie 테이블
+            movie.setActor("aaaa");
+            movie.setDirector("bbbb");
+
+            // Item 테이블
+            movie.setName("제목제목");
+            movie.setPrice(10000);
+            em.persist(movie);
+
+            em.flush();
+            em.clear();
+
+            Movie findMovie = em.find(Movie.class, movie.getId());
+            System.out.println("findMovie = " + findMovie.getName());
+
+//            Item item = em.find(Item.class, movie.getId());
+//            System.out.println("item = " + item);
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- maven-jpa/src/main/java/jpabook/jpashop/domain/Item.java
+++ maven-jpa/src/main/java/jpabook/jpashop/domain/Item.java
@@ -4,26 +4,23 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Setter
 @Entity
-public class Item {
+// @Inheritance(strategy = InheritanceType.JOINED) // 부모 클래스에서 작성해야 한다. (조인 전략)
+// @Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 단일 테이블 전략
+// 단일 테이블 전략은 @DiscriminatorColumn 어노테이션을 작성하지 않더라도 DTYPE 컬림이 반드시 생성된다.
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS) // 구현 클래스마다 테이블 전략
+// @DiscriminatorColumn // DTYPE 컬럼 생성 (기본 컬럼명 DTYPE, 저장되는 값 default로 각각의 엔티티명이 들어감 -> 자식 클래스에서 지정 가능)
+// @DiscriminatorColumn(name = "DIS_TYPE") // 이런식으로 컬럼명을 지정해 줄 수도 있다.
+public abstract class Item { // abstract 클래스로 변경
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ITEM_ID")
+    @Id // @GeneratedValue(strategy = GenerationType.IDENTITY) // <- 구현 클래스마다 테이블 전략 사용 불가 (특히 MySQL)
     private Long id;
 
     private String name;
 
     private int price;
-
-    private int stockQuantity;
-
-    @ManyToMany(mappedBy = "items") // 추가
-    private List<Category> categories = new ArrayList<>();
 
 }
